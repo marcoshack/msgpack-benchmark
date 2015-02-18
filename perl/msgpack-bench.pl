7014649594abd9de469e240292beec337a8ae1ff
Lucas Mateus Fiel de Oliveira <lucas.oliveira0@wal-mart.com>
2015-02-18 12:23:10 -0200

    implemented a real Benchmark

--- perl/msgpack-bench.pl
+++ perl/msgpack-bench.pl
@@ -2,136 +2,125 @@
 
 use strict;
 use warnings;
-use Time::HiRes qw/ gettimeofday tv_interval /;
 use Data::MessagePack;
 use JSON::XS;
+use Benchmark qw/ :hireswallclock cmpthese /;
 
-my ($d1, $d2);
-my $runTests = 1000;
-my %rTimes = ();
+my $json     = JSON::XS->new;
+my $msgpack  = Data::MessagePack->new;
 
-my $stringData = {"glossary"=>{"title"=>"example glossary","GlossDiv"=>{"title"=>"S","GlossList"=>{"GlossEntry"=>{"ID"=>"SGML","SortAs"=>"SGML","GlossTerm"=>"Standard Generalized Markup Language","Acronym"=>"SGML","Abbrev"=>"ISO 8879=>1986","GlossDef"=>{"para"=>"A meta-markup language, used to create markup languages such as DocBook.","GlossSeeAlso"=>["GML","XML"]},"GlossSee"=>"markup"}}}}};
+my $stringData = {
+    "glossary" => {
+        "title"    => "example glossary",
+        "GlossDiv" => {
+            "title"     => "S",
+            "GlossList" => {
+                "GlossEntry" => {
+                    "ID"        => "SGML",
+                    "SortAs"    => "SGML",
+                    "GlossTerm" => "Standard Generalized Markup Language",
+                    "Acronym"   => "SGML",
+                    "Abbrev"    => "ISO 8879=>1986",
+                    "GlossDef"  => {
+                        "para" =>
+                            "A meta-markup language, used to create markup languages such as DocBook.",
+                        "GlossSeeAlso" => [ "GML", "XML" ]
+                    },
+                    "GlossSee" => "markup"
+                }
+            }
+        }
+    }
+};
 
 my $numberData = [
-  [1, 2, 3, 4, -1, 1, undef],
-  [3, 6, 5, 4,  1, 0, 7],
-  [3, 2, 8, 1,  0, 1, 0],
-  [10, 11, 12, 13,  14, 0, 1],
-  [15, 16, 17, 18, 19, 1, undef],
-  [20, 21, 22, 23,  24, undef, 7],
-  [25, 26, 27, 28,  29, undef, 0],
-  100, 200, 300,
-  [
-    [1, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0],
-    [0, 0, 1, 0, 0],
-    [0, 0, 0, 1, 0],
-    [0, 0, 0, 0, 1]
-  ]
+    [ 1,  2,  3,  4,  -1, 1,     undef ],
+    [ 3,  6,  5,  4,  1,  0,     7 ],
+    [ 3,  2,  8,  1,  0,  1,     0 ],
+    [ 10, 11, 12, 13, 14, 0,     1 ],
+    [ 15, 16, 17, 18, 19, 1,     undef ],
+    [ 20, 21, 22, 23, 24, undef, 7 ],
+    [ 25, 26, 27, 28, 29, undef, 0 ],
+    100, 200, 300,
+    [   [ 1, 0, 0, 0, 0 ],
+        [ 0, 1, 0, 0, 0 ],
+        [ 0, 0, 1, 0, 0 ],
+        [ 0, 0, 0, 1, 0 ],
+        [ 0, 0, 0, 0, 1 ]
+    ]
 ];
 
-my $misto = {
+my $alphaNumber = {
     stringData => $stringData,
     numberData => $numberData
 };
 
-my $bigData = {};
+my $big = {};
 
-for (0..100) {
-    $bigData->{'data'.$_} = {
-        stringData => $stringData,
-        numberData => $numberData
+for(0..1000) {
+    $big->{ 'data_' . $_ } = $alphaNumber;
+}
+
+print "\nCompare STRING data only.\n";
+
+### String
+cmpthese(
+    -1,
+    {   'json_string' => sub {
+            my $result = $json->decode( $json->encode($stringData) );
+        },
+        'msgpack_string' => sub {
+            my $result = $msgpack->decode( $msgpack->encode($stringData) );
+        }
     }
-}
+);
 
-my $json = JSON::XS->new;
-my $msgpack = Data::MessagePack->new;
+print "\n\nCompare NUMBERS data only.\n";
 
+## Numbers
+cmpthese(
+    -1,
+    {   'json_number' => sub {
+            my $result = $json->decode( $json->encode($numberData) );
+        },
+        'msgpack_number' => sub {
+            my $result = $msgpack->decode( $msgpack->encode($numberData) );
+        }
+    }
+);
 
-## MsgPack String
-$d1 = [gettimeofday];
-for (1..$runTests) {
-    my $result = $msgpack->decode($msgpack->encode($stringData));
-}
-$d2 = [gettimeofday];
+print "\n\nCompare STRING and NUMBERS data.\n";
 
-$rTimes{msgpack_string} = int($runTests/tv_interval($d1, $d2));
+## alphaNumber
+cmpthese(
+    -1,
+    {   'json_alphaNumber' => sub {
+            my $result = $json->decode( $json->encode($alphaNumber) );
+        },
+        'msgpack_aplhaNumber' => sub {
+            my $result = $msgpack->decode( $msgpack->encode($alphaNumber) );
+        }
+    }
+);
 
-## Json String
-$d1 = [gettimeofday];
-for (1..$runTests) {
-    my $result = $json->decode($json->encode($stringData));
-}
-$d2 = [gettimeofday];
+print "\n\nCompare a BIG data (1000 x string and numbers data size).\n";
 
-$rTimes{json_string} = int($runTests/tv_interval($d1, $d2));
+## Big
+cmpthese(
+    -1,
+    {   'json_big' => sub {
+            my $result = $json->decode( $json->encode($big) );
+        },
+        'msgpack_big' => sub {
+            my $result = $msgpack->decode( $msgpack->encode($big) );
+        }
+    }
+);
 
+print "\n";
+exit 1;
 
-
-## MsgPack Number
-$d1 = [gettimeofday];
-for (1..$runTests) {
-    my $result = $msgpack->decode($msgpack->encode($numberData));
-}
-$d2 = [gettimeofday];
-
-$rTimes{msgpack_number} = int($runTests/tv_interval($d1, $d2));
-
-## Json Number
-$d1 = [gettimeofday];
-for (1..$runTests) {
-    my $result = $json->decode($json->encode($numberData));
-}
-$d2 = [gettimeofday];
-
-$rTimes{json_number} = int($runTests/tv_interval($d1, $d2));
-
-
-
-## MsgPack Misto
-$d1 = [gettimeofday];
-for (1..$runTests) {
-    my $result = $msgpack->decode($msgpack->encode($misto));
-}
-$d2 = [gettimeofday];
-
-$rTimes{msgpack_misto} = int($runTests/tv_interval($d1, $d2));
-
-## Json Misto
-$d1 = [gettimeofday];
-for (1..$runTests) {
-    my $result = $json->decode($json->encode($misto));
-}
-$d2 = [gettimeofday];
-
-$rTimes{json_misto} = int($runTests/tv_interval($d1, $d2));
-
-
-
-## MsgPack BigData
-$d1 = [gettimeofday];
-for (1..$runTests) {
-    my $result = $msgpack->decode($msgpack->encode($bigData));
-}
-$d2 = [gettimeofday];
-
-$rTimes{msgpack_big} = int($runTests/tv_interval($d1, $d2));
-
-## Json BigData
-$d1 = [gettimeofday];
-for (1..$runTests) {
-    my $result = $json->decode($json->encode($bigData));
-}
-$d2 = [gettimeofday];
-
-$rTimes{json_bigdata} = int($runTests/tv_interval($d1, $d2));
-
-
-
-
-foreach my $k (sort { $rTimes{$b} <=> $rTimes{$a} } keys %rTimes) {
-    printf("%s \t .......  %d op/sec\n", $k, $rTimes{$k});
-}
+__END__
 
 
 
